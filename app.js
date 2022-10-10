3f41dcef9488b9bc9d016e58e02c3c9ef71ef81b
Astha Negi <asthanegi539@gmail.com>
2022-10-11 02:04:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+console.log("hi");
+var words = ['Graphic Era Deemded To be University ','Graphic Era Hill University Dehradun', 'Graphic Era Hill University Bheemtal', 'Graphic Era Hill University Haldwani'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.MainHeading').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
